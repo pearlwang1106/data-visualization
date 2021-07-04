3e96178aa78fbf2bf6ec375b2a75c7cb018a1fa0
nana <502539421@qq.com>
2021-07-04 12:19:15 +0800

    fix: 提取echarts公共组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,24 +2,51 @@
   el-container
     el-header 
       HeaderCom
-    el-main
+    el-main.home-box
       el-row
-        el-col(:span='6') 左
-        el-col(:span='12') 中
-        el-col(:span='6') 右1
+        el-col(
+          :span='6',
+          class='home-box-left'
+        )
+          demo
+        el-col(
+          :span='12',
+          class='home-box-center'
+        ) 中
+        el-col(
+          :span='6',
+          class='home-box-right'
+        ) 右1
 </template>
 
 <script>
 import HeaderCom from '../../components/header';
+import demo from '../../components/charts/demo';
 
 export default {
   name: 'home',
   components: {
-    HeaderCom
+    HeaderCom,
+    demo
   }
 }
 </script>
 
 <style lang="less">
-
+.home-box {
+  // width: 100%;
+  width: 2880px;
+  height: auto;
+  .home-box-left {
+    height: auto;
+    background-color:#ccc;
+  }
+  .home-box-center {
+    height: auto;
+  }
+  .home-box-right {
+    height: auto;
+    background-color:#000;
+  }
+}
 </style>
